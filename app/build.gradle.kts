b7d94bb6c49e70786116b5c611d98f135863a879
chyngyzturap <chyngyzturapov@gmail.com>
2024-04-19 09:47:43 +0600

    LC, Coil

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.pharos.composeprofilecard"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.pharos.composeprofilecard"
@@ -51,14 +51,16 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material3:material3:1.1.1")
+    implementation("androidx.compose.runtime:runtime-livedata:1.6.5")
+    implementation("io.coil-kt:coil-compose:2.4.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/pharos/composeprofilecard/MainActivity.kt
+++ app/src/main/java/com/pharos/composeprofilecard/MainActivity.kt
@@ -3,44 +3,155 @@ package com.pharos.composeprofilecard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
-import com.pharos.composeprofilecard.ui.theme.ComposeProfileCardTheme
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.pharos.composeprofilecard.ui.theme.MyTheme
+import com.pharos.composeprofilecard.ui.theme.LightGreen200
+import com.pharos.composeprofilecard.ui.theme.LightRed200
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeProfileCardTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+            MyTheme {
+                MainScreen()
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    @Composable
+    fun MainScreen(userProfiles: List<UserProfile> = userProfileList) {
+        Scaffold(topBar = { AppBar() }) {
+            Surface(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(it),
+            ) {
+                LazyColumn() {
+                    items(userProfiles) { profile ->
+                        ProfileCard(userProfile = profile)
+                    }
+                }
+            }
+        }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ComposeProfileCardTheme {
-        Greeting("Android")
+    }
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun AppBar() {
+        TopAppBar(
+            navigationIcon = {
+                Icon(
+                    imageVector = Icons.Default.Home,
+                    contentDescription = "App Bar",
+                    modifier = Modifier.padding(horizontal = 12.dp),
+                )
+            },
+            title = { Text(text = "Chat App") },
+            colors = TopAppBarDefaults.topAppBarColors(Color.White)
+        )
+    }
+
+    @Composable
+    fun ProfileCard(userProfile: UserProfile) {
+        Card(
+            modifier = Modifier
+                .padding(top = 16.dp, bottom = 4.dp, start = 16.dp, end = 16.dp)
+                .fillMaxWidth(),
+            elevation = CardDefaults.cardElevation(8.dp),
+            colors = CardDefaults.cardColors(Color.White)
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                userProfile.run {
+                    ProfilePicture(photoUrl, userProfile.isActive)
+                    ProfileContent(name, isActive)
+                }
+
+            }
+        }
+    }
+
+    @Composable
+    fun ProfileContent(name: String, isActive: Boolean) {
+        Column(
+            modifier = Modifier
+                .padding(8.dp)
+                .fillMaxWidth(),
+        ) {
+            Text(
+                text = name,
+                style = MaterialTheme.typography.headlineSmall,
+                modifier = Modifier.alpha(if (isActive) 1f else 0.5f)
+            )
+            Text(
+                text = if (isActive) "active now" else "offline",
+                style = MaterialTheme.typography.bodyMedium,
+                modifier = Modifier.alpha(if (isActive) 1f else 0.5f)
+            )
+
+        }
+    }
+
+    @Composable
+    fun ProfilePicture(
+        photoUrl: String,
+        isActive: Boolean
+    ) {
+        Card(
+            shape = CircleShape,
+            border = BorderStroke(
+                width = 2.dp,
+                color = if (isActive) LightGreen200 else LightRed200
+            ),
+            modifier = Modifier.padding(16.dp),
+            elevation = CardDefaults.cardElevation(4.dp)
+        ) {
+            AsyncImage(
+                model = photoUrl,
+                contentDescription = "Profile Photo",
+                modifier = Modifier.size(72.dp),
+                contentScale = ContentScale.Crop
+            )
+        }
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun Preview() {
+        MyTheme {
+            MainScreen()
+        }
     }
 }
